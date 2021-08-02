ad6e3c39fb6e432d2224c9e8414d8592ecaba7a2
Jungik <wjddlr0303@gmail.com>
2021-08-02 11:31:32 +0900

    Add quick_sort

---
+++ sort/quick_sort.py
@@ -0,0 +1,16 @@
+array = [5, 7, 9, 0, 3, 1, 6, 2, 4, 8]
+
+def quick_sort(array):
+    # 리스트가 하나 이하의 원소만을 담고 있다면 종료
+    if len(array) <= 1:
+        return array
+
+    pivot = array[0] # 첫 번째 원소를 피벗으로 설정
+    tail = array[1:] # 피벗을 제외한 리스트
+
+    left = [x for x in tail if x <= pivot]
+    right = [x for x in tail if x > pivot]
+
+    return quick_sort(left) + [pivot] + quick_sort(right)
+
+print(quick_sort(array))
